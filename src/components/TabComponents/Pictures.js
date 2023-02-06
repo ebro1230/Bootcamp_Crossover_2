8ae83c4f256f42abb9c25c3e46dc3cab97a422ac
MirkoDuric <mirkodjuricczv@gmail.com>
2023-02-06 20:13:10 +0100

    Pics renderingwhen you change restaurant

--- src/components/TabComponents/Pictures.js
+++ src/components/TabComponents/Pictures.js
@@ -1,7 +1,10 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 export default function Pictures({ restaurant }) {
   const [pics, setPics] = useState(restaurant.pics);
   console.log(pics);
+  useEffect(() => {
+    setPics(restaurant.pics);
+  }, [restaurant]);
   return (
     <div className="img-tab-container">
       {pics.map((pic) => {
