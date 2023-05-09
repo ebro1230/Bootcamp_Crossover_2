2c28d91e662d2de917e7cae0da13e37c07828e42
ebro1230 <ebro1230@gmail.com>
2023-05-09 22:25:35 +0200

    fixing homepage nav

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,7 +6,7 @@ function Sidebar({ restaurants }) {
     <aside className="sidebarNav">
       <NavLink
         className="navlinks homepage"
-        to="/"
+        to="/Bootcamp_Crossover_2/"
         style={({ isActive }) => ({ color: isActive ? "#00bcd4" : null })}
       >
         Homepage
