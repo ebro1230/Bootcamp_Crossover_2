9ecc27205434f69fbfbe65febe92afe141b8f11f
ebro1230 <ebro1230@gmail.com>
2023-05-09 23:08:18 +0200

    let's see

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,12 +4,13 @@ import { NavLink, Outlet } from "react-router-dom";
 function Sidebar({ restaurants }) {
   return (
     <aside className="sidebarNav">
-      <a
+      <NavLink
         className="navlinks homepage"
-        href="https://ebro1230.github.io/Bootcamp_Crossover_2"
+        to="https://ebro1230.github.io/Bootcamp_Crossover_2"
+        style={({ isActive }) => ({ color: isActive ? "#00bcd4" : null })}
       >
         Homepage
-      </a>
+      </NavLink>
       <hr />
       {restaurants.map((restaurant) => {
         return (
